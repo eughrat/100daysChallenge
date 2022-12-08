6c73c69c2adecfe502b6395d2fbb6e1d2b9f4747
eughrat <michal.piernicki@gmail.com>
2022-12-08 16:26:20 +0100

    doing blackjack

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -11,12 +11,25 @@ def pick_a_card(deck):
     deck.append(card)
     return deck
 
+def check_if_win(deck):
+    if sum(deck) == 21:
+        return True
+    elif 11 in deck and sum(deck) > 21:
+        idx = deck.index(11)
+        deck[idx] = 1
+        if sum(deck) == 21:
+            return True
+    else:
+        return False
+
+
+
 play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
 while play == "y":
+    player_deck = pick_a_card(player_deck)
+    player_deck = pick_a_card(player_deck)
     while sum(player_deck) <=21:
-        player_deck = pick_a_card(player_deck)
-        player_deck = pick_a_card(player_deck)
         print(logo)
         print(f"Your cards: {player_deck}, current score: {sum(player_deck)}")
         computer_deck = pick_a_card(computer_deck)
@@ -25,4 +38,6 @@ while play == "y":
         draw_a_card = input("Type 'y' to get another card, type 'n' to pass: ")
         if draw_a_card == "y":
             player_deck = pick_a_card(player_deck)
+            player_win = check_if_win(player_deck)
+
 
