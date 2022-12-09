30dd847eb98b5c3aef347e335ad9b1d13594e943
eughrat <michal.piernicki@gmail.com>
2022-12-09 14:23:39 +0100

    doing blackjack - second attempt

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -11,7 +11,7 @@ def pick_a_card(deck):
     deck.append(card)
     return deck
 
-def check_if_win(deck):
+def check_player(deck):
     if sum(deck) == 21:
         return True
     elif 11 in deck and sum(deck) > 21:
@@ -22,22 +22,62 @@ def check_if_win(deck):
     else:
         return False
 
-
-
 play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
 
 while play == "y":
     player_deck = pick_a_card(player_deck)
     player_deck = pick_a_card(player_deck)
-    while sum(player_deck) <=21:
-        print(logo)
+    computer_deck = pick_a_card(computer_deck)
+    computer_deck = pick_a_card(computer_deck)
+    print(logo)
+    print(f"Your cards: {player_deck}, current score: {sum(player_deck)}")
+    print(f"Computer's first card: {computer_deck[0]}")
+    draw_a_card = input("Type 'y' to get another card, type 'n' to pass: ")
+    while draw_a_card == 'y':
+        player_deck = pick_a_card(player_deck)
         print(f"Your cards: {player_deck}, current score: {sum(player_deck)}")
-        computer_deck = pick_a_card(computer_deck)
-        computer_deck = pick_a_card(computer_deck)
         print(f"Computer's first card: {computer_deck[0]}")
         draw_a_card = input("Type 'y' to get another card, type 'n' to pass: ")
-        if draw_a_card == "y":
-            player_deck = pick_a_card(player_deck)
-            player_win = check_if_win(player_deck)
+    if draw_a_card == 'n':
+        while sum(computer_deck) <=21:
+            computer_deck = pick_a_card(computer_deck)
+            if sum(compu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while play == "y":
+#     player_deck = pick_a_card(player_deck)
+#     player_deck = pick_a_card(player_deck)
+#     while sum(player_deck) <=21:
+#         print(logo)
+#         print(f"Your cards: {player_deck}, current score: {sum(player_deck)}")
+#         computer_deck = pick_a_card(computer_deck)
+#         computer_deck = pick_a_card(computer_deck)
+#         print(f"Computer's first card: {computer_deck[0]}")
+#         draw_a_card = input("Type 'y' to get another card, type 'n' to pass: ")
+#         if draw_a_card == "y":
+#             player_deck = pick_a_card(player_deck)
+#             player_win = check_player(player_deck)
+#             if check_player == True:
+#                 while sum(computer_deck) <= 21:
+#                     computer_deck = pick_a_card(computer_deck)
+#                     computer_win = check_player(computer_deck)
+
 
 
