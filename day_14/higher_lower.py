6bcba4cafcd435a0700411580331662103d832c2
eughrat <michal.piernicki@gmail.com>
2022-12-15 21:04:28 +0100

    ended with higher lower game

--- day_14/higher_lower.py
+++ day_14/higher_lower.py
@@ -1,18 +1,35 @@
 import random
 from game_data import data
 from art import logo, vs
+from replit import clear
 
 print(logo)
 
 def select_rivals(data):
     data_idxs = [i for i in range(len(data))]
-
     first_rival = data[random.choice(data_idxs)]
-    second_rival = data[random.randint(0, len(data) - 1)]
-    while first_rival == second_rival:
-        first_rival = data[random.randint(0, len(data) - 1)]
-        second_rival = data[random.randint(0, len(data) - 1)]
-
+    data.remove(first_rival)
+    second_rival = data[random.choice(data_idxs)]
     return first_rival, second_rival
 
-print(select_rivals(data))
+play = True
+points = 0
+
+while play:
+    first_rival, second_rival = select_rivals(data)
+    print(f"Compare A {first_rival['name']}, a {first_rival['description']}, from {first_rival['country']}")
+    print(vs)
+    print(f"Compare A {second_rival['name']}, a {second_rival['description']}, from {second_rival['country']}")
+    choice = (input("Who has more followers? Type 'A' or 'B': ")).upper()
+    if choice == "A" and first_rival["follower_count"] > second_rival["follower_count"]:
+        points += 1
+        print(f"You're right! Current score: {points}")
+        clear()
+    elif choice == "B" and first_rival["follower_count"] < second_rival["follower_count"]:
+        points += 1
+        print(f"You're right! Current score: {points}")
+        clear()
+    else:
+        print(f"Sorry, that's wrong. Final score: {points}")
+        clear()
+        break
