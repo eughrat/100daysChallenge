31bb8afb957e5d7f9e0edb53ecfece3fd8de1052
eughrat <michal.piernicki@gmail.com>
2022-12-30 15:33:06 +0100

    doing day 29

--- day_29/main.py
+++ day_29/main.py
@@ -7,13 +7,14 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 photo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=photo)
 canvas.grid(column=1, row=0)
 
+# LABELS
 website_label = Label(text="Website")
 website_label.grid(column=0, row=1)
 
@@ -23,12 +24,23 @@ username_label.grid(column=0, row=2)
 password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
 
+# ENTRIES
 website_input = Entry(width=35)
+website_input.focus()
 website_input.grid(column=1, row=1, columnspan=2)
 
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+username_input = Entry(width=35)
+username_input.grid(column=1, row=2, columnspan=2)
+username_input.insert(0, "michal.piernicki@gmail.com")
 
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
 
+# BUTTONS
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
