c9218aaef88ca2ac87eb30808de98e339b96019b
eughrat <michal.piernicki@gmail.com>
2022-12-31 17:59:05 +0100

    ended day 29 with password manager project!

--- day_27/main.py
+++ day_27/main.py
@@ -1,31 +1,29 @@
 from tkinter import *
 
+window = Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+
+my_label = Label(text="I'm a label", font=('Arial', 24, "bold"))
+my_label.pack()
+my_label["text"] = "new text"
+
+# Button
 
 def button_clicked():
-    print("I got clicked")
     new_text = input.get()
     my_label.config(text=new_text)
 
+button = Button(text="Click me", command=button_clicked)
+button.pack()
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+# Entry
 
-my_label = Label(text="I am a Lbel", font=("Arial", 24, "bold"))
-my_label.config(text="New Text")
-my_label.grid(column=0, row=0)
-my_label.config(padx=100, pady=200)
+input = Entry()
+input.pack()
 
-button = Button(text="Click on me", command=button_clicked)
-button.grid(column=1, row=1)
 
-new_button = Button(text="New Button", command=button_clicked)
-new_button.grid(column=2, row=0)
-# button.pack()
-
-input = Entry(width=10)
-print(input.get())
-input.grid(column=3, row=2)
-# input.pack()
 
 window.mainloop()
--- day_29/main.py
+++ day_29/main.py
@@ -1,8 +1,45 @@
 from tkinter import *
+from tkinter import messagebox
+import string
+import random
+import pyperclip
+
+letters_lower = [i for i in string.ascii_lowercase]
+letters_upper = [i for i in string.ascii_uppercase]
+digits = [i for i in string.digits]
+punctuation = [i for i in string.punctuation]
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def password_generator():
+    let_low = random.choices(letters_lower, k=3)
+    let_up = random.choices(letters_upper, k=3)
+    dig = random.choices(digits, k=3)
+    punc = random.choices(punctuation, k=3)
+    password = let_low + let_up + dig + punc
+    random.shuffle(password)
+    password = ''.join(password)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_password():
+    if len(website_input.get()) == 0 or len(password_input.get()) == 0 or len(username_input.get()) == 0:
+        messagebox.showinfo(title="Alert!",
+                            message=f"There are blanks left to fill in!")
+    else:
+        is_ok = messagebox.askquestion(title=website_input.get(),
+                                       message=f"These are the details entered: \nEmail: {username_input.get()}"
+                                               f" \nPassword: {password_input.get()} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as f:
+                f.write(f"{website_input.get()} | {username_input.get()} | {password_input.get()}\n")
+            website_input.delete(0, 'end')
+            password_input.delete(0, 'end')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -37,10 +74,10 @@ password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
 # BUTTONS
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=password_generator)
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
