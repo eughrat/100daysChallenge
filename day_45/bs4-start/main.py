94f3955514b7a0458067d4c3e1e863b0690d48f0
eughrat <michal.piernicki@gmail.com>
2023-01-16 20:21:37 +0100

    doing day 45

--- day_45/bs4-start/main.py
+++ day_45/bs4-start/main.py
@@ -27,10 +27,49 @@ yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 print(soup.title)
 
-all_anchor_tags = soup.find_all(class_="titleline")
+title_tags = soup.find_all(class_="titleline")
+score_tags = soup.find_all(class_="score")
+link_tags = soup.find_all(class_="sitestr")
 
-for tag in all_anchor_tags:
-    print(tag.get_text())
+print(len(title_tags))
+print(len(score_tags))
+print(len(link_tags))
+
+titles = []
+scores = []
+links = []
+
+for i in range(len(title_tags)):
+    try:
+        title = title_tags[i].get_text()
+        titles.append(title)
+        # print(title_tags[i].get_text())
+        score = score_tags[i].get_text()
+        scores.append(score)
+        # print(score_tags[i].get_text())
+        link = link_tags[i].get_text()
+        links.append(link)
+        # print(link_tags[i].get_text())
+    except IndexError:
+        continue
+
+for i in range(len(scores)):
+    points = scores[i].split(" ")
+    scores[i] = int(points[0])
+
+highest_score_idx = scores.index(max(scores))
+print(titles[highest_score_idx])
+print(links[highest_score_idx])
+
+
+print(titles)
+print(scores)
+print(links)
+
+
+# for tag in title_tags:
+#     print(tag.get_text())
+#     print(tag.get("href"))
 
 
 
